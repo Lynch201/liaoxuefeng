fa7b9593039dd00edac30c6216cc0fa765f28e0b
Lynch201 <lynch201xdu@gmail.com>
2016-09-25 23:46:07 +0800

    vcs

---
+++ SMTP_sendEmail.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# coding:utf-8
+# 201609252252
+# lynch.wang
+
+from email.mime.text import MIMEText
+msg = MIMEText('hello,send by python', 'plain', 'utf-8')
+
+#from_addr = input('From:')
+from_addr  = '1092376202@qq.com'
+#password = input('Password: ')
+password = 'gyct bpbl ynrd bada'
+
+#to_addr = input('To: ')
+to_addr = 'lynch201xdu@gmail.com'
+#smtp_server = input('SMTP server: ')
+smtp_server = 'smtp.qq.com'
+
+
+import smtplib
+server = smtplib.SMTP_SSL(smtp_server, 465)
+server.set_debuglevel(1)
+
+try:
+	server.login(from_addr, password)
+	server.sendmail(from_addr, [to_addr], msg.as_string())
+except smtplib.SMTPAuthenticationError as e:
+	#print(e)
+	e_str = str(e)
+	errordescription = e_str[5:len(e_str)-1]
+	print(errordescription)
+
+finally:
+	server.quit()
+print('finish')
---
+++ ServerUDP.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding:utf-8
+#201609251728
+#lynch.wang
+
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(('127.0.0.1', 9999))
+print('Bind UDP on 9999')
+while True:
+	data,addr = s.recvfrom(1024)
+	print('Receive from %s:%s' % addr)
+	s.sendto(b'Hello, %s!' % data, addr)
+
+
