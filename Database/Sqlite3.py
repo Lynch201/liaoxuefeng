1e66ceaecb7207cda009c1fa261afefe343cfefb
Lynch201 <lynch201xdu@gmail.com>
2016-09-26 11:47:24 +0800

    def get_score_in(low, high):     ' 返回指定分数区间的名字，按分数从低到高排序 '
      finish

--- Database/Sqlite3.py
+++ Database/Sqlite3.py
@@ -13,21 +13,36 @@ if os.path.isfile(db_file):
 # 初始数据：
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
-try:
-	cursor.execute('create table user(id varchar(20) primary key, name varchar(20), score int)')
-	cursor.execute(r"insert into user values ('A-001', 'Adam', 95)")
-	cursor.execute(r"insert into user values ('A-002', 'Bart', 65)")
-	cursor.execute(r"insert into user values ('A-003', 'Lisa', 78)")
-finally:
-	cursor.close()
-	conn.commit()
-	conn.close()
+
+cursor.execute('create table user(id varchar(20) primary key, name varchar(20), score int)')
+cursor.execute(r"insert into user values ('A-001', 'Adam', 95)")
+cursor.execute(r"insert into user values ('A-002', 'Bart', 65)")
+cursor.execute(r"insert into user values ('A-003', 'Lisa', 78)")
+
+cursor.close()
+conn.commit()
+conn.close()
+
 
 
 def get_score_in(low, high):
 	""" 返回指定分数区间的名字， 按分数从低到高排列 """
-	pass
+	conn = sqlite3.connect(db_file)
+	cursor = conn.cursor()
+	try:
+		cursor.execute('select * from user where score between ? and ? order by score', (low, high))
 
+		values = cursor.fetchall()
+		#print(values)
+	finally:
+		cursor.close()
+		conn.commit()
+		conn.close()
+
+	#print([x[1] for x in values])
+	return [x[1] for x in values]
+
+# get_score_in(0, 100)
 
 # test
 assert get_score_in(80, 95) == ['Adam'], get_score_in(80, 95)
