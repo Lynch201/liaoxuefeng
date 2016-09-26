0a5c3191f4c26854a89857832cb2d843c6334de1
Lynch201 <lynch201xdu@gmail.com>
2016-09-26 09:11:33 +0800

    send email from gmail to qqmail no authority

--- SMTP_sendEmail.py
+++ SMTP_sendEmail.py
@@ -7,30 +7,32 @@ from email.mime.text import MIMEText
 msg = MIMEText('hello,send by python', 'plain', 'utf-8')
 
 #from_addr = input('From:')
-from_addr  = '1092376202@qq.com'
+from_addr  = 'lynch201xdu@gamil.com'
 #password = input('Password: ')
-password = 'gyctbpblynrdbada'
+password = 'gg2937w9'
 
 #to_addr = input('To: ')
-to_addr = 'lynch201xdu@gmail.com'
+to_addr = '1092376202@qq.com'
 #smtp_server = input('SMTP server: ')
-smtp_server = 'smtp.gamil.com'
+smtp_server = 'smtp.qq.com'
 
 
 import smtplib
-server = smtplib.SMTP_SSL(smtp_server, 465)
+# the smtp_port for qqmail is 465 and need smtp.SMTP_SSL()
+# and gmail is 587
+smtp_port = 587
+server = smtplib.SMTP(smtp_server, smtp_port)
+server.starttls()
 server.set_debuglevel(1)
 
 try:
 	server.login(from_addr, password)
 	server.sendmail(from_addr, [to_addr], msg.as_string())
 except smtplib.SMTPAuthenticationError as e:
-	#print(e)
-	e_str = str(e)
-	errordescription = e_str[5:len(e_str)-1]
-	print(errordescription)
+	print(e)
+	errorCode, explaning = e
+	print(explaning.decode('utf-8'))
 	# 就是想读出来那个bytes类型的字符串到底是个啥
-
 finally:
 	server.quit()
 print('finish')
